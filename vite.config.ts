e4f6afc49e50d7518a55dca877a20b1d291fdb34
Humaam-04-06 <humaamahmed40@gmail.com>
2026-09-06 03:34:41 -0700

    feat(ci): configure vite relative base path and production build asset bundling

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === 'true' ? '/QueryLens/' : './',
   plugins: [react()],
   resolve: {
     alias: {
@@ -13,4 +14,24 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['sql.js'],
   },
+  build: {
+    chunkSizeWarningLimit: 3500,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          icons: [
+            '@fortawesome/react-fontawesome',
+            '@fortawesome/free-solid-svg-icons',
+            '@fortawesome/free-regular-svg-icons',
+            '@fortawesome/free-brands-svg-icons',
+          ],
+          'sql-parser': ['node-sql-parser', 'sql-formatter'],
+          diagram: ['@xyflow/react'],
+        },
+      },
+    },
+  },
+
 });
+
